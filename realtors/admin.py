48f083d72fc3cd6e4a1a72fdffb156cd7a21597a
Kelello Kekana <Kelello.Kekana@naspers.onmicrosoft.com>
2020-05-05 19:44:21 +0200

    Initial commit

---
+++ realtors/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Realtor
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','hire_date')
+
+admin.site.register(Realtor)
